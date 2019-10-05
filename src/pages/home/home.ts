4e5cf317f91863c542077ee8de0f9ea539ec9d4d
Elton Dornelas <eltondornelas@gmail.com>
2019-10-04 22:26:52 -0300

    Navegacao

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,8 +10,17 @@ import { NavController, IonicPage } from 'ionic-angular';
 })
 export class HomePage {
 
+  //navCtrl é um objeto do tipo NavController. Para injetar (no Angular ou TS) uma dependência ou uma instancia de um objeto dentro da classe, você declara como parâmetro no seu Construtor
   constructor(public navCtrl: NavController) {
 
+  }
+
+  public login() {
+    //o this é uma obrigação no TS
+    //this.navCtrl.push('CategoriasPage');
+    this.navCtrl.setRoot('CategoriasPage');
+    //empilha uma página em cima da outra.
+    //com o push, ele automaticamente coloca a setinha para voltar porém, nesse caso não queremos que ele volte para a tela de login, então vamos tirar o push
   }
 
 }
