5abd6fd1d59ccc28cd31edc97ec0a9b3294d4523
Elton Dornelas <eltondornelas@gmail.com>
2019-10-07 08:34:10 -0300

    Mostrando as categorias

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,5 +1,6 @@
 //definindo configurações da API, expressando como objeto json.
 
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080"
+    baseUrl: "http://localhost:8080",
+    bucketBaseUrl: "https://curso-spring-ionic-esd.s3-sa-east-1.amazonaws.com"
 }
