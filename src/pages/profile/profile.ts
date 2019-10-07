56cab4ee34a88f3c1a6077b21f041620787d8828
Elton Dornelas <eltondornelas@gmail.com>
2019-10-07 20:39:24 -0300

    Tratando erros 403

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -42,7 +42,14 @@ export class ProfilePage {
         this.cliente = response;
         this.getImageIfExists(); //busca imagem do Bucket do S3.
       },
-      error => {});
+      error => {
+        if (error.status == 403) {
+          this.navCtrl.setRoot('HomePage');
+        }
+      });
+  }
+  else {
+    this.navCtrl.setRoot('HomePage');
     }    
   }
 
