efa8444657a6d7e819d9a999142b544527b5573e
Elton Dornelas <eltondornelas@gmail.com>
2019-10-08 15:01:36 -0300

    Componente loading

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ProdutoDTO } from '../../models/produto.dto';
 import { API_CONFIG } from '../../config/api.config';
 import { ProdutoService } from '../../services/domain/produto.service';
+import { LoadingController } from 'ionic-angular/components/loading/loading-controller';
 
 @IonicPage()
 @Component({
@@ -16,17 +17,33 @@ export class ProdutosPage {
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
-    public produtoService: ProdutoService) {
+    public produtoService: ProdutoService,
+    public loadingCtrl: LoadingController) {
   }
 
   ionViewDidLoad() {
     let categoria_id = this.navParams.get('categoria_id');
+    let loader = this.presentLoading();
+    //resolver iniciar o loader antes de fazer a requisição do backend para carregar os produtos
     this.produtoService.findByCategoria(categoria_id)
       .subscribe(response => {
         this.items = response['content'];
+        loader.dismiss();
         this.loadImageUrls();
       },
-      error => {});
+      error => {
+        loader.dismiss();
+      });
+  }
+
+  //aquele velho loading screen...
+  presentLoading() {
+    let loader = this.loadingCtrl.create({
+      content: "Aguarde..."
+      //duration: 3000. não vamos colocar o duration, vamos deixar com duração enquanto realmente estiver carregando a página
+    });
+    loader.present();
+    return loader;
   }
 
   loadImageUrls() {
