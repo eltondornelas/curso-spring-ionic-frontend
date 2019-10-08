97e325c0c91c0cae4126fdeb5ebf40dfe94a3f93
Elton Dornelas <eltondornelas@gmail.com>
2019-10-08 11:45:58 -0300

    Tela de escolha de endereco

--- src/models/cidade.dto.ts
+++ src/models/cidade.dto.ts
@@ -1,4 +1,8 @@
+import { EstadoDTO } from "./estado.dto";
+
 export interface CidadeDTO {
     id : string;
     nome : string;
+    estado? : EstadoDTO;
+    //campo opcional para ele não interferir de mostrar as cidades sem estar os estados anexado as cidades
 }
